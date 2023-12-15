6e8aabaa548318736f4c7bb6c6b38e7d401094e0
Y. Orçun Gökbulut <orcun.gokbulut@gmail.com>
2023-12-15 15:25:20 +0300

    ADD: Divided into modules.

--- init.lua
+++ init.lua
@@ -8,108 +8,17 @@ vim.cmd("set nowrap")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system(
+        {
+            "git",
+            "clone",
+            "--filter=blob:none",
+            "https://github.com/folke/lazy.nvim.git",
+            "--branch=stable",
+            lazypath,
+        }
+    )
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-    { "nvim-telescope/telescope.nvim", tag = "0.1.5", dependencies = { "nvim-lua/plenary.nvim" }},
-    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-    {
-        "nvim-neo-tree/neo-tree.nvim",
-        branch = "v3.x",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-            "MunifTanjim/nui.nvim",
-            -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-        }
-    }
-}
-
-
-local opts = {}
-require("lazy").setup(plugins, opts)
-
-require("catppuccin").setup()
-vim.cmd.colorscheme "catppuccin"
-
-local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-
-local config = require("nvim-treesitter.configs")
-config.setup(
-    {
-        ensure_installed = { 
-            "lua", 
-            "vim", 
-            "vimdoc", 
-            "query", 
-            "awk", 
-            "bash",
-            "c",
-            "cpp",
-            "c_sharp",
-            "css", 
-            "html", 
-            "cmake", 
-            "cpp", 
-            "css", 
-            "cuda",
-            "diff",
-            "dockerfile", 
-            "git_config", 
-            "git_rebase", 
-            "gitattributes", 
-            "gitignore", 
-            "gitcommit", 
-            "glsl",
-            "hlsl",
-            "http",
-            "html",
-            "ini",
-            "java",
-            "javascript",
-            "jsdoc",
-            "json",
-            "latex",
-            "llvm",
-            "lua",
-            "luadoc",
-            "make",
-            "markdown",
-            "objdump",
-            "passwd",
-            "pem",
-            "perl",
-            "php",
-            "qmldir",
-            "qmljs",
-            "regex",
-            "solidity",
-            "sql",
-            "ssh_config",
-            "strace",
-            "typescript",
-            "udev",
-            "vim",
-            "vimdoc",
-            "vue",
-            "xml",
-            "yaml"
-        },
-        sync_install = false,
-        highlight = true,
-        indent = true
-    }
-)
-
-vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle left<CR>", {})
+require("lazy").setup("plugins")
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,73 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+       local config = require("nvim-treesitter.configs")
+        config.setup(
+            {
+                ensure_installed = { 
+                    "lua", 
+                    "vim", 
+                    "vimdoc", 
+                    "query", 
+                    "awk", 
+                    "bash",
+                    "c",
+                    "cpp",
+                    "c_sharp",
+                    "css", 
+                    "html", 
+                    "cmake", 
+                    "cpp", 
+                    "css", 
+                    "cuda",
+                    "diff",
+                    "dockerfile", 
+                    "git_config", 
+                    "git_rebase", 
+                    "gitattributes", 
+                    "gitignore", 
+                    "gitcommit", 
+                    "glsl",
+                    "hlsl",
+                    "http",
+                    "html",
+                    "ini",
+                    "java",
+                    "javascript",
+                    "jsdoc",
+                    "json",
+                    "latex",
+                    "llvm",
+                    "lua",
+                    "luadoc",
+                    "make",
+                    "markdown",
+                    "objdump",
+                    "passwd",
+                    "pem",
+                    "perl",
+                    "php",
+                    "qmldir",
+                    "qmljs",
+                    "regex",
+                    "solidity",
+                    "sql",
+                    "ssh_config",
+                    "strace",
+                    "typescript",
+                    "udev",
+                    "vim",
+                    "vimdoc",
+                    "vue",
+                    "xml",
+                    "yaml"
+                },
+                sync_install = false,
+                highlight = true,
+                indent = true
+            }
+        )
+    end
+}
+    
